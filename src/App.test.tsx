655efa3e877cb2e3b47807200eefcb2a787cc1a9
homfarnam <homfarnam@gmail.com>
2021-08-28 01:05:55 +0430

    Create snapshot test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import toJson from 'enzyme-to-json';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+afterEach(cleanup);
+
+describe('renders learn react link', () => {
+  it('renders correctly enzyme, return app to json, snappshot test', () => {
+    const wrapper = Enzyme.shallow(<App />);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
 });
