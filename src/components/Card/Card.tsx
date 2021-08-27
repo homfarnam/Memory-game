45bc877dccfcafe699b85bdaca9aaeb95ac3ad65
homfarnam <homfarnam@gmail.com>
2021-08-28 00:34:16 +0430

    Create cart component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,35 @@
+import classnames from 'classnames';
+import './card.scss';
+
+interface CardProps {
+  item: {
+    id: number;
+    url: string;
+    first_name: string;
+  };
+  whichItem: (index: number) => void;
+  index: number;
+  visible?: boolean;
+}
+
+const Card: React.FC<CardProps> = ({ item, index, whichItem, visible }) => {
+  const handleClick = () => {
+    if (!visible) {
+      whichItem(index);
+    }
+  };
+
+  return (
+    <div
+      key={item.id}
+      onClick={handleClick}
+      className={classnames('card', {
+        'is-flipped': visible,
+        'is-inactive': !visible,
+      })}>
+      <img src={item.url} alt='' className='h-[200px] w-[200px]' draggable={false} />
+    </div>
+  );
+};
+
+export default Card;
