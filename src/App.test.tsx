0e6d6b0cc23f6c50b31d9d6042404b9052e7e3a0
homfarnam <homfarnam@gmail.com>
2021-08-28 23:53:40 +0430

    Write new test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import { cleanup } from '@testing-library/react';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import toJson from 'enzyme-to-json';
 import Game from './pages/Game';
 import Home from './pages/Home';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import userEvent from '@testing-library/user-event';
 
 Enzyme.configure({ adapter: new Adapter() });
 afterEach(cleanup);
@@ -19,5 +22,19 @@ describe('snapshot test', () => {
     const wrapper = Enzyme.shallow(<Game />);
 
     expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  it('test home page have a title', () => {
+    const history = createMemoryHistory();
+
+    const { getAllByText } = render(
+      <Router history={history}>
+        <Home />
+      </Router>
+    );
+
+    const text = getAllByText('Click the Memory Card! Game');
+
+    expect(text).toBeTruthy();
   });
 });
