b8fd9e8bf4433237cb75ed212523ee25d91144c5
homfarnam <homfarnam@gmail.com>
2021-08-28 22:21:01 +0430

    Add new test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
-import App from './App';
-import Enzyme, { shallow } from 'enzyme';
+import { cleanup } from '@testing-library/react';
+import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import toJson from 'enzyme-to-json';
+import Game from './pages/Game';
+import Home from './pages/Home';
 
 Enzyme.configure({ adapter: new Adapter() });
 afterEach(cleanup);
 
-describe('renders learn react link', () => {
-  it('renders correctly enzyme, return app to json, snappshot test', () => {
-    const wrapper = Enzyme.shallow(<App />);
+describe('snapshot test', () => {
+  it('snapshot test Home page', () => {
+    const wrapper = Enzyme.shallow(<Home />);
+
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+  it('snapshot test Game page', () => {
+    const wrapper = Enzyme.shallow(<Game />);
 
     expect(toJson(wrapper)).toMatchSnapshot();
   });
