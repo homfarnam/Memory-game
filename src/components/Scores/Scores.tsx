1aeb942e4bb1944a569a12596d5e3f6d7a63977d
homfarnam <homfarnam@gmail.com>
2021-08-28 22:09:11 +0430

    Create score component

---
+++ src/components/Scores/Scores.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+interface ScoresProps {
+  usersData:
+    | {
+        id: number;
+        user: string;
+        date: string;
+        score: string;
+      }[]
+    | undefined;
+}
+
+const Scores: React.FC<ScoresProps> = ({ usersData }) => {
+  return (
+    <div className="w-full flex justify-center">
+      {usersData?.length ? (
+        <table className="border border-black">
+          <thead className="p-2">
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Date</th>
+            <th>Score</th>
+          </thead>
+          <tbody className="p-2">
+            {usersData?.map((item, i) => (
+              <tr key={item?.id}>
+                <td>{i + 1}</td>
+                <td>{item?.user}</td>
+                <td>{item?.date}</td>
+                <td>{item?.score}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        'There is no data'
+      )}
+    </div>
+  );
+};
+
+export default Scores;
