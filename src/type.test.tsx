ec368b15013538d0b2aead6b5026308ac6cda6d6
homfarnam <homfarnam@gmail.com>
2021-08-29 12:02:23 +0430

    Add new test

--- src/type.test.tsx
+++ src/type.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 
 import Home from './pages/Home';
 import { Router } from 'react-router-dom';
@@ -21,4 +21,28 @@ test('type on input', async () => {
   await waitFor(() => {
     expect(input).toHaveValue('farnam');
   });
+});
+
+test('click on button', async () => {
+  const history = createMemoryHistory();
+
+  const result = render(
+    <Router history={history}>
+      <Home />
+    </Router>
+  );
+
+  const input = screen.getByLabelText(/User/i);
+
+  userEvent.type(input, 'farnam');
+
+  await waitFor(() => {
+    expect(input).toHaveValue('farnam');
+
+    fireEvent.submit(input);
+  });
+
+  const someElement = result.container.querySelector('#user');
+
+  expect(someElement?.innerHTML).toBe('Hello farnam');
 });
