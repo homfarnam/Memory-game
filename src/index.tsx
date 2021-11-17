5b07e7f031dced6a936a0a43547e65e72a9caf7e
homfarnam <homfarnam@gmail.com>
2021-11-17 23:15:42 +0330

    Import css

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from "react-dom"
-import "./styles/index.css"
-import App from "./App"
-import * as serviceWorker from "./serviceWorker"
 import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+import "./styles/index.css"
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 ReactDOM.render(
   <BrowserRouter>
@@ -10,8 +10,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
